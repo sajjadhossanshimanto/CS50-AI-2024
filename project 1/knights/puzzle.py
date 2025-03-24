85b1895aad3909c9252c501902bc10ee80377e8d
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-03-24 13:46:21 +0600

    hw doen upon propositional logic

--- project 1/knights/puzzle.py
+++ project 1/knights/puzzle.py
@@ -9,24 +9,43 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+
+kb = And(
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave))
+)
+
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    kb,
+    Implication(AKnight, And(AKnight, AKnave)),
+    Implication(AKnave, Not(And(AKnight, AKnave)))
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    kb,
+    Implication(AKnight, (And(AKnave, BKnave))),
+    Implication(AKnave, Not(And(AKnave, BKnave)))
+    # And(Not(BKnave), Not(BKnight))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    kb,
+    Implication(AKnight, Or(And(AKnave, BKnave), And(AKnight, BKnight))),
+    Implication(AKnave, Not(Or(And(AKnave, BKnave), And(AKnight, BKnight)))),
+    Implication(BKnight, Or(And(BKnight, AKnave), And(BKnave, AKnight))),
+    Implication(BKnave, Not(Or(And(BKnight, AKnave), And(BKnave, AKnight))))
 )
 
 # Puzzle 3
@@ -35,7 +54,23 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    kb,
+    # a says
+    Implication(AKnight, And(Or(AKnight, AKnave))),
+    Implication(AKnave, Not(And(Or(AKnight, AKnave)))),
+    
+    # b says
+    Or(Implication(BKnight, Or(Implication(AKnight, AKnave), Implication(AKnave, Not(AKnave)))),
+    Implication(BKnave, Not(Or(Implication(AKnight, AKnave), Implication(AKnave, Not(AKnave))))),),
+    # Implication(BKnave, Not(BKnave)),
+
+    # b says
+    Implication(BKnight, CKnave),
+    Implication(BKnave, Not(CKnave)),
+
+    # c says
+    Implication(CKnight, AKnight),
+    Implication(CKnave, Not(AKnight)),
 )
 
 
